ca2a29679fa275a4bd523e1ab39f5cd7fa23612a
uphoon <uphoon@gmail.com>
2022-05-15 16:34:25 +0900

    change base href

---
+++ docs/lifeNumberABI.js
@@ -0,0 +1,20 @@
+var lifeNumberABI = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "getNumbers",
+		"outputs": [
+			{
+				"internalType": "uint256[]",
+				"name": "",
+				"type": "uint256[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
